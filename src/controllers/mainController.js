e4f237caf394b6119d5e7e32da256f9eaecba85b
Joaquin Abal <joabal97@gmail.com>
2022-05-04 17:28:09 -0300

    creacion footer, creacion contactForm, head agregue link de fontAwesome

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,3 +5,16 @@ const controlador = {
 }
 
 module.exports = controlador;
+/* 
+const objeto = {
+    obj1: 'joa',
+    obj2: 23,
+    obj3: true,
+    obj4: [
+        {
+            miniObj: 'hola'
+        }
+    ]
+}
+
+objeto.obj3[0].miniObj */
